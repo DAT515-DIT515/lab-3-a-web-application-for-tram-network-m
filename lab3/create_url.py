358dd2316a1e6b5c0b0f268b148fb1f46b0df687
andyvungoc <149588666+andyvungoc@users.noreply.github.com>
2023-12-13 20:12:29 +0100

    Update create_url.py

--- lab3/create_url.py
+++ lab3/create_url.py
@@ -6,16 +6,13 @@ from urllib.parse import urlencode, urlparse, parse_qs
 STOP_FILE = 'https://www.vasttrafik.se/reseplanering/hallplatslista/'
 TRAM_FILE = '/Users/andyvungoc/PycharmProjects/DAT515/lab3/static/tramnetwork.json'
 
-# Load tram network data
 with open(TRAM_FILE, 'r') as tramfile:
     tram_data = json.load(tramfile)
-
-# Extract unique stops from tram network data
+    
 stops = set()
 for line in tram_data['lines']:
     stops.update(tram_data['lines'][line])
 
-# Fetch stop information from the given URL
 response = requests.get(STOP_FILE)
 response.raise_for_status()
 
@@ -33,7 +30,6 @@ for row in range(len(lines)):
     else:
         pass
 
-# Save the URLs to JSON file
 output_file = 'static/tram-url.json'
 with open(output_file, 'w') as file:
     json.dump(stops_code, file, indent=2, ensure_ascii=False)
